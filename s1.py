3e8cd1ea8f9289de1f5dc9a3b3eb3de9baf24c5c
R morel <38894494+mrl893@users.noreply.github.com>
2024-04-06 20:16:53 -0400

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,4 +1,96 @@
 ltipledata.py
+
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(
+    "ML/london_merged.csv",
+    parse_dates=["timestamp"],
+    index_col="timestamp"
+)
+
+
+
+
+print(df.shape)
+
+df["hour"] = df.index.hour
+df["day_of_month"] = df.index.day
+df["day_of_week"] = df.index.dayofweek
+df["month"] = df.index.month
+
+train_size = int(len(df) * 0.9)
+test_size = len(df) - train_size
+train, test = df.iloc[0:train_size], df.iloc[train_size: len(df)]
+print(len(train), len(test))
+"""
+f_columns = ["t1", "t2", "hum", "wind_speed"]
+
+
+f_transformer = RobustScaler()
+
+f_transformer = f_transformer.fit(train[f_columns].to_numpy())
+train.loc[:, f_columns] = f_transformer.transform(
+    train[f_columns].to_numpy()
+)
+
+
+
+cnt_transformer = RobustScaler()
+
+cnt_transformer = cnt_transformer.fit(train[["cnt"]])
+
+train["cnt"] = cnt_transformer.transform(train[['cnt']])
+test["cnt"] = cnt_transformer.transform(test[["cnt"]])
+
+"""
+
+def create_dataset(X, y, time_steps=1):
+    Xs, ys = [],[]
+    for i in range(len(X)- time_steps):
+        v = X.iloc[i:(i + time_steps)].values
+        Xs.append(v)
+        ys.append(y.iloc[i+ time_steps])
+    return np.array(Xs), np.array(ys)
+
+
+time_steps= 10
+
+X_train, y_train = create_dataset(train, train.cnt, time_steps)
+X_test, y_test = create_dataset(test, test.cnt, time_steps)
+
+print(X_train.shape, y_train.shape)
+
+
+model = keras.Sequential()
+model.add(
+    keras.layers.Bidirectional(
+        keras.layers.LSTM(
+            units=128,
+            input_shape=(X_train.shape[1], X_train.shape[2])
+        )
+    )
+)
+model.add(keras.layers.Dropout(rate=0.2))
+model.add(keras.layers.Dense(units=1))
+model.compile(loss="mean_squared_error", optimizer="adam")
+
+
+history = model.fit(
+    X_train, y_train,
+    epochs=30,
+    batch_size=32,
+    validation_split=0.1,
+    shuffle=False
+)
+
+
 2024-04-06 20:03:33.592390: I tensorflow/core/platform/cpu_feature_guard.cc:210] This TensorFlow binary is optimized to use available CPU instructions in performance-critical operations.
 To enable the following instructions: AVX2 FMA, in other operations, rebuild TensorFlow with the appropriate compiler flags.
 (17414, 9)
