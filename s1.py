8b3093bf66aef5298c6c4849c62febdef4ae2143
R morel <38894494+mrl893@users.noreply.github.com>
2024-04-06 20:13:02 -0400

    Create s1.py

---
+++ s1.py
@@ -0,0 +1,66 @@
+ltipledata.py
+2024-04-06 20:03:33.592390: I tensorflow/core/platform/cpu_feature_guard.cc:210] This TensorFlow binary is optimized to use available CPU instructions in performance-critical operations.
+To enable the following instructions: AVX2 FMA, in other operations, rebuild TensorFlow with the appropriate compiler flags.
+(17414, 9)
+15672 1742
+(15662, 10, 13) (15662,)
+Epoch 1/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 11s 16ms/step - loss: 2080340.6250 - val_loss: 3155923.0000
+Epoch 2/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 1880403.7500 - val_loss: 2929727.2500
+Epoch 3/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 1721388.6250 - val_loss: 2728351.0000
+Epoch 4/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 1580580.8750 - val_loss: 2545737.2500
+Epoch 5/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 1454246.5000 - val_loss: 2376370.5000
+Epoch 6/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 8s 18ms/step - loss: 1339937.1250 - val_loss: 2220948.5000
+Epoch 7/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 11s 25ms/step - loss: 1238088.5000 - val_loss: 2079599.1250
+Epoch 8/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 10s 23ms/step - loss: 1146696.6250 - val_loss: 1949873.1250
+Epoch 9/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 14ms/step - loss: 1064352.8750 - val_loss: 1830131.0000
+Epoch 10/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 14ms/step - loss: 988852.6875 - val_loss: 1719311.0000
+Epoch 11/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 14ms/step - loss: 921107.1250 - val_loss: 1616908.3750
+Epoch 12/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 859774.3750 - val_loss: 1519629.2500
+Epoch 13/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 8s 19ms/step - loss: 808587.8125 - val_loss: 1452054.2500
+Epoch 14/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 8s 17ms/step - loss: 753347.4375 - val_loss: 1350570.7500
+Epoch 15/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 14s 32ms/step - loss: 693657.1875 - val_loss: 1262651.8750
+Epoch 16/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 8s 17ms/step - loss: 640803.8750 - val_loss: 1176551.0000
+Epoch 17/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 12s 22ms/step - loss: 592779.8750 - val_loss: 1105287.5000
+Epoch 18/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 14ms/step - loss: 552603.2500 - val_loss: 1035857.9375
+Epoch 19/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 14ms/step - loss: 516372.2188 - val_loss: 972441.0625
+Epoch 20/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 481552.9688 - val_loss: 918839.5000
+Epoch 21/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 10s 13ms/step - loss: 450101.0625 - val_loss: 876709.9375
+Epoch 22/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 14ms/step - loss: 418523.1250 - val_loss: 808862.3125
+Epoch 23/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 393357.9375 - val_loss: 768758.3125
+Epoch 24/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 6s 13ms/step - loss: 367421.6875 - val_loss: 721877.1250
+Epoch 25/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 9s 20ms/step - loss: 344192.1875 - val_loss: 683095.0625
+Epoch 26/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 7s 15ms/step - loss: 324309.9375 - val_loss: 650538.4375
+Epoch 27/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 7s 16ms/step - loss: 305331.3125 - val_loss: 617016.8750
+Epoch 28/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 12s 20ms/step - loss: 284005.5938 - val_loss: 584151.3750
+Epoch 29/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 7s 16ms/step - loss: 268940.5312 - val_loss: 545715.1250
+Epoch 30/30
+441/441 ━━━━━━━━━━━━━━━━━━━━ 7s 17ms/step - loss: 253708.3750 - val_loss: 522654.5000
